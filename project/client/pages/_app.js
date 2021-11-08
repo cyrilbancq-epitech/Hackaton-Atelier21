a05a59c266fc693d522a2f4343428c330f2efdc5
David Chmarzynski <david.chmarzynski@epitech.eu>
2021-11-09 00:00:21 +0100

    Header + Footer

--- project/client/pages/_app.js
+++ project/client/pages/_app.js
@@ -2,6 +2,8 @@ import '../styles/globals.css';
 
 // IMPORT STYLES
 import "../styles/index.scss";
+import "../styles/header.scss";
+import "../styles/footer.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
